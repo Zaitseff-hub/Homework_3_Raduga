c4d49fee9c22516b86c56703238fe63ba906c214
Zaitseff-hub <az.pomocnik@gmail.com>
2023-01-24 16:22:06 +0100

    Initial commit

---
+++ src/com/company/Person1.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Person1 {
+    public static void main(String[] args) {
+
+        Integer i = new Integer(682);
+
+        Double d = new Double(2.33);
+
+        Boolean b = new Boolean(false);
+
+        byte num = 23;
+        int num_1 = 34656748;
+
+        float num_2 = 23.45f;
+        double num_3 = 234.123456789d;
+
+        boolean isTrue = true;
+        char sym = 'R';
+
+        String str = "Hello World!";
+
+        System.out.println(str);
+
+    }
+}
